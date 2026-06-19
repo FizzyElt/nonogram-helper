4e9765de8df8d30801213863b16052a8e8da2d99
FizzyElt <fizzyelt.dev@outlook.com>
2026-06-20 01:13:00 +0800

    change formatter

---
+++ oxfmt.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+    ignorePatterns: [],
+    tabWidth: 4,
+    printWidth: 100,
+    endOfLine: "lf",
+    semi: true,
+    singleQuote: false,
+    sortImports: {
+        groups: [
+            "type-import",
+            ["value-builtin", "value-external"],
+            "type-internal",
+            "value-internal",
+            ["type-parent", "type-sibling", "type-index"],
+            ["value-parent", "value-sibling", "value-index"],
+            "unknown",
+        ],
+    },
+    sortTailwindcss: true,
+    svelte: true,
+});
---
+++ oxlint.config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from "oxlint";
+
+export default defineConfig({
+    plugins: ["oxc", "typescript", "unicorn", "vitest", "promise", "eslint", "import"],
+    categories: {
+        correctness: "error",
+        restriction: "warn",
+    },
+    rules: {
+        "import/no-default-export": "allow",
+        "oxc/no-async-await": "off",
+        "oxc/no-barrel-file": "off",
+        "oxc/no-optional-chaining": "off",
+        "oxc/no-rest-spread-properties": "off",
+        "typescript/explicit-function-return-type": "off",
+        "eslint/no-undefined": "off",
+    },
+
+    settings: {
+        "jsx-a11y": {
+            components: {},
+            attributes: {},
+        },
+        next: {
+            rootDir: [],
+        },
+        react: {
+            formComponents: [],
+            linkComponents: [],
+            componentWrapperFunctions: [],
+        },
+        jsdoc: {
+            ignorePrivate: false,
+            ignoreInternal: false,
+            ignoreReplacesDocs: true,
+            overrideReplacesDocs: true,
+            augmentsExtendsReplacesDocs: false,
+            implementsReplacesDocs: false,
+            exemptDestructuredRootsFromChecks: false,
+            tagNamePreference: {},
+        },
+        vitest: {
+            typecheck: false,
+        },
+    },
+    env: {
+        builtin: true,
+    },
+    globals: {},
+    ignorePatterns: [],
+});
